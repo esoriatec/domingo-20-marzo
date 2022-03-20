94656ad0eb20544c93463eed44c51b7985382b74
esoria <edu@esoriatec.com>
2022-03-20 19:08:17 +0100

    Creacion de componentes

---
+++ src/componentes/Clientes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Facturas() {
+  return (
+    <div>Clientes</div>
+  )
+}
+
+export default Clientes
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <BrowserRouter>
+   <Routes>
+     <Route path="/" element={<Home></Home>}/> 
+     <Route path="/facturas" element={<Facturas></Facturas>}/> 
+     <Route path="/clientes" element={<Clientes></Clientes>}/> 
+     <Route path="/Productos" element={<Productos></Productos>}/> 
+   </Routes>
+ </BrowserRouter>,
   document.getElementById('root')
 )
