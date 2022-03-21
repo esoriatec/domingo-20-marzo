ef59ff2ccb7af00603b58bd7d91b8f1f32d9a724
esoria <edu@esoriatec.com>
2022-03-21 18:00:16 +0100

    OK hasta ver estado de las cuentas

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -3,11 +3,12 @@ import {Link} from 'react-router-dom'
 function Header() {
   return (
     <header >
-     
-        <ul className="list-group list-group-horizontal">
+        <ul className='list-group list-group-horizontal'>
             <li ClassName="list-group-item"><Link to="/clientes">Clientes</Link></li>
             <li ClassName="list-group-item"><Link to="/productos">Productos</Link></li>
             <li ClassName="list-group-item"><Link to="/facturas">Facturas</Link></li>
+            <li ClassName="list-group-item"><Link to="/eth">Eth</Link></li>
+
         </ul>
       
     </header>
