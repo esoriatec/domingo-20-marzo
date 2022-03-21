8781b4f1e7e3cc13435f48f00063fdfcd158b367
esoria <edu@esoriatec.com>
2022-03-21 19:59:07 +0100

    con factturas por fecha

--- src/componentes/Facturas.jsx
+++ src/componentes/Facturas.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import {useQuery} from 'react-query';
+ 
+const URL_API = "https://my-bdd-server-nlngw9uco-jviejo.vercel.app"
+
+function getFactura() {
+  return axios.get(`${URL_API}/sql?sql=select customers.company_name,customers.country, orders.order_date from CUSTOMERS inner join  ORDERS on  customers.CUSTOMER_ID = orders.CUSTOMER_ID and extract(year from order_date)='1997'`);
+}
 
 export default function Facturas() {
-  return (
-    <div>Facturas</div>
+ //Query
+ 
+ const { isLoading, data } = useQuery('factura', getFactura)
+  
+if (isLoading) {
+  return <div>Espere un momento...por favor</div>
+}
+ 
+
+return (
+    <div>
+    <h1>Facturas</h1>
+    <table className="table">
+      <thead>
+        <tr>
+          <td>compañía</td>
+          <td>País</td>
+          <td>Fecha</td>
+        </tr>
+      </thead>
+      <tbody>
+        {data.data.map(factura =>(
+          <tr key={factura.company_name}>
+            <td>{factura.company_name}</td>
+            <td>{factura.country}</td>
+            <td>{factura.order_date}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
+    </div>
   )
 }
